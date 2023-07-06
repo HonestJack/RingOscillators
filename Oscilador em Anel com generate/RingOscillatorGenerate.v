7e5180a93fd8640b37db8cde69f7b59d724a6ea8
HonestJack <mucareinicke@gmail.com>
2023-07-06 16:01:03 -0300

    Created a top level module and defined the board pins

--- Oscilador em Anel com generate/RingOscillatorGenerate.v
+++ Oscilador em Anel com generate/RingOscillatorGenerate.v
@@ -1,13 +1,14 @@
 module inverter
 	(
 		input in,
-		output reg out
+		output out
 	);
 	
-	always @ (*)
-	begin
-		out <= ~in;
-	end
+	assign out = ~in;
+	
+//	always @ (*) begin
+//		out <= ~in;
+//	end
 	
 endmodule
 
@@ -26,10 +27,9 @@ module RingOscillatorBase
 		always @ (*) begin
 			and_1 <= en & notGate[N-1];
 			notGate[0] <= ~and_1;
-			for (i = 1; i < N; i = i +1)
-			begin: inverter_chain
-				//notGate[i] <= ~notGate[i - 1];
-				inverter inv(notGate[i - 1], notGate[i]);
+			for (i = 1; i < N; i = i +1) begin
+				notGate[i] <= ~notGate[i - 1];
+				//inverter inv(notGate[i - 1], notGate[i]);
 			end
 		end
 	endgenerate
@@ -38,10 +38,16 @@ endmodule
 module RingOscillatorGenerate
 	(
 		input en,
-		output out3, out5, out7
+		output run,
+		output out3, out5, out7, out101, out1001, out4999
 	);
+	
+	assign run = en;
 	
 	RingOscillatorBase #(.N(3)) ring3(en, out3);
 	RingOscillatorBase #(.N(5)) ring5(en, out5);
 	RingOscillatorBase #(.N(7)) ring7(en, out7);
+	RingOscillatorBase #(.N(101)) ring101(en, out101);
+	RingOscillatorBase #(.N(1001)) ring1001(en, out1001);
+	RingOscillatorBase #(.N(4999)) ring4999(en, out4999);
 endmodule
